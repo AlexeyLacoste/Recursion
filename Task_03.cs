acd05ef4903cb4570c8d170121b3c3a1744f6d3b
AlexeyLacoste <148151717+AlexeyLacoste@users.noreply.github.com>
2023-12-14 19:59:42 +0300

    Add files via upload

---
+++ Task_03.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Program {
+ 
+ static void Main()
+    {
+        
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] arr, int i)
+    {
+        if (i >= 0)
+        {
+            Console.WriteLine(arr[i]);
+            PrintArrayReverse(arr, i - 1);
+        }
+    }
+}
---
+++ Task_2.cs
@@ -0,0 +1,34 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение m:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+
+        Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+
+       
+    }
+
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+}
